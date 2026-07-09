ae77e77178c259968c3aca0709b2fad265142715
Harentsoa Penielà <harentsoapeniela@gmail.com>
2026-07-09 13:14:10 +0200

    Second version of the app

--- script/app.js
+++ script/app.js
@@ -2,6 +2,17 @@ const faqsHeaders = Array.from(document.querySelectorAll('.faq__header'))
 
 for (const faqHeader of faqsHeaders) {
     faqHeader.addEventListener('click', () => {
+         const otherFaqsHeaders = faqsHeaders.filter(f => f !== faqHeader)
+         
+        for (const otherFaqHeader of otherFaqsHeaders) {
+            const otherFaqDescription = otherFaqHeader.parentElement.querySelector('.faq__description')
+            if (otherFaqDescription.classList.contains('show')) {
+                otherFaqDescription.classList.remove('show')
+                otherFaqHeader.querySelector('img').setAttribute('src', './assets/images/icon-plus.svg')
+
+            }
+        }
+       
         const faqDescription = faqHeader.parentElement.querySelector('.faq__description')
         if (faqDescription.classList.contains('show')) {
             faqHeader.querySelector('img').setAttribute('src', './assets/images/icon-plus.svg')
@@ -10,6 +21,6 @@ for (const faqHeader of faqsHeaders) {
             faqHeader.querySelector('img').setAttribute('src', './assets/images/icon-minus.svg')
             faqDescription.classList.add('show')
         }
-        
+
     })
 }
